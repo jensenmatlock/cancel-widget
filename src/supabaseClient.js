ff402eb01632916a8da6ba6981f3e3579529bc86
Jensen Matlock <jensen.matlock@gmail.com>
2025-06-13 14:59:19 -0400

    Update before rebase

--- src/supabaseClient.js
+++ src/supabaseClient.js
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
 
-const SUPABASE_URL = "https://sdhhujiktuqldbbeczyy.supabase.co";
-const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkaGh1amlrdHVxbGRiYmVjenl5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDkwNzQyMjEsImV4cCI6MjA2NDY1MDIyMX0.bYqQhptKU6-QjC_8YY_ieirpj-kaT3wLTNklPb-hYlM";
+// Use environment variables for safety
+const SUPABASE_URL = import.meta.env.VITE_SUPABASE_URL;
+const SUPABASE_ANON_KEY = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
